6c93095a67c85ab160cf9970eada3422acff60d9
Robert <rjgriffin42@gmail.com>
2015-10-27 00:28:17 +0000

    got the driver working as a standalone module runable with sudo privelages

--- joint_estimation/scripts/mag3110_driver.py
+++ joint_estimation/scripts/mag3110_driver.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-import rospy
+#import rospy
 
-from joint_estimation.msg import AxesValues 
-from Adafruit_I2C import Adafruit_I2C
+#from joint_estimation.msg import AxesValues 
+from Adafruit_I2C import Adafruit_I2C # FIXME
 
 # Mag 3310 Driver Class
 class Mag_Driver():
@@ -11,47 +11,55 @@ class Mag_Driver():
   def __init__(self):
     # Get the ~private namespace parameters from command line or launch file
     print "get parameters"
-    rate = float(rospy.get_param('~rate', '10.0'))
+    #rate = float(rospy.get_param('~rate', '10.0')) FIXME
     #address = rospy.get_param('~address', '0x60')
     
 
     # Create message object
-    #i2c = Adafruit_I2C(address)
-    i2c = Adafruit_I2C(0x60)
+    address = 0x60
+    DEVICE_REG = 0
+    i2c = Adafruit_I2C(address) # FIXME
 
-    # Create a publisher for magnet messages
-    pub = rospy.Publisher("magnet_topic", AxesValues)
-    msg = AxesValues()
-    msg.x_axis = 0
-    msg.y_axis = 0
-    msg.z_axis = 0
+    # Create a publisher for magnet messages FIXME
+    #pub = rospy.Publisher("magnet_topic", AxesValues)
+    #msg = AxesValues()
+    #msg.x_axis = 0
+    #msg.y_axis = 0
+    #msg.z_axis = 0
 
     print "Initialize"
 
     # Main while loop
-    while not rospy.is_shutdown():
+    while True:
+    #while not rospy.is_shutdown(): #FIXME
       print "Main loop"
-      # read and populate message from board
-      byte = i2c.readU8()
-      msg.x = (byte[0]<<8) + byte[1]
-      msg.y = (byte[2]<<8) + byte[3]
-      msg.z = (byte[4]<<8) + byte[5]
+      i2c.write8(DEVICE_REG, 0x01)
+
+      # read in six bytes of message
+      data = []
+      # Make list of 6 bytes
+      for i in range (0, 6):
+        data.append(i2c.readU8(DEVICE_REG))
+
+      x = (data[0] << 8) + data[1]
+      y = (data[2] << 8) + data[3]
+      z = (data[4] << 8) + data[5]
       
       # publish data out
-      pub.publish(msg)
+      #pub.publish(msg) #FIXME
      
       # Sleep for a while before publishing new messages
-      if rate:
-          rospy.sleep(1/rate)
-      else:
-          rospy.sleep(1.0)
+      #if rate:
+      #    rospy.sleep(1/rate)
+      #else:
+      #    rospy.sleep(1.0)
 
 # Main function
 if __name__ == '__main__':
-  rospy.init_node('mag3110_driver')
+  #rospy.init_node('mag3110_driver') #FIXME
   print "calling main"
   # Go to class functions that do all the heavy lifting. Do error checking.
-  try:
-      print "to driver"
-      md = Mag_Driver()  
-  except rospy.ROSInterruptException: pass
+  #try:
+  print "to driver"
+  md = Mag_Driver()  
+  #except rospy.ROSInterruptException: pass # FIXME
