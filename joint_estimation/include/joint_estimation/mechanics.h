a12c8d1601d70b3b197da92f3d69974caa728286
Robert <rjgriffin42@gmail.com>
2015-10-28 15:50:56 +0000

    updated the mechanics file. this needs to be filled out

--- joint_estimation/include/joint_estimation/mechanics.h
+++ joint_estimation/include/joint_estimation/mechanics.h
@@ -3,7 +3,6 @@
 
 #define NO_SENSORS 1
 
-void mechanics_compute_forward_kinematics(float sensor_position[NO_SENSORS], float sensor_velocity[NO_SENSORS]);
-void mechanics_compute_inverse_kinematics(float joint_position, float joint_velocity);
+void mechanics_compute_inverse_kinematics(float sensor_position[NO_SENSORS], float sensor_velocity[NO_SENSORS], float joint_position, float joint_velocity);
 
 #endif
--- joint_estimation/src/mechanics.cpp
+++ joint_estimation/src/mechanics.cpp
@@ -2,15 +2,17 @@
 #include <joint_estimation/vector3d.h>
 
 static const vector3d sensor_top[NO_SENSORS] = {
-  {} // sensor one
+  {0.0f, 0.0f, 0.0f} // sensor one
 };
 
 static const vector3d sensor_bot[NO_SENSORS] = {
-  {} // sensor one
+  {0.0f, 0.0f, 0.0f} // sensor one
 };
 
-void mechanic_compute_forward_kinematics()
-{}
+static const vector3d joint_axis[NO_SENSORS] = {
+  {0.0f, 1.0f, 0.0f} // sensor one
+};
 
-void mechanics_compute_inverse_kinematics()
-{}
+void mechanics_compute_inverse_kinematics(float sensor_position[NO_SENSORS], float sensor_velocity[NO_SENSORS], float joint_position, float joint_velocity)
+{
+}
