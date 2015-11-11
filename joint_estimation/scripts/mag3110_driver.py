a85956af9e1c9ce1bcdcc09e81d138e351c9145f
Robert <rjgriffin42@gmail.com>
2015-11-11 19:38:00 +0000

    got the driver publishing out data

--- joint_estimation/scripts/mag3110_driver.py
+++ joint_estimation/scripts/mag3110_driver.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import time
 import rospy
 from joint_estimation.msg import AxesValues
 from Adafruit_I2C import Adafruit_I2C
@@ -9,11 +10,11 @@ class Mag_Driver():
   def __init__(self):
     # Get the ~private namespace parameters from command line or launch file
     rate = float(rospy.get_param('~rate', '10.0'))
-    address = rospy.get_param('~address', '0x60')
+    address = rospy.get_param('~address', '0x0e')
     no_sensors = rospy.get_param('~no_sensors', 1)
 
     # Create message object
-    address = 0x60
+    address = 0x0e
     DEVICE_REG = 1
     i2c = Adafruit_I2C(address)
 
@@ -30,18 +31,42 @@ class Mag_Driver():
 
     # Main while loop
     while not rospy.is_shutdown():
-      i2c.write8(DEVICE_REG, 0x01) #FIXME is this needed?
+      #initialize bus
+      i2c.write8(17,128)
+      i2c.write8(16,1)
+      data = [0, 0, 0, 0, 0, 0]
 
-      # read in six bytes of message FIXME have this iterate of the different nodes
-      data = []
-      # Make list of 6 bytes
-      for i in range (0, 6):
-        data.append(i2c.readU8(DEVICE_REG))
+      time.sleep(0.0002) 
+  
+      #read x
+      i2c.write8(1,0)
+      time.sleep(0.0002)
+      data[0] = i2c.readU8(1)
+      time.sleep(0.0002)
+      i2c.write8(2,0)
+      time.sleep(0.0002)
+      data[1] = i2c.readU8(2)
+      msg.x_axis[0] = data[1] + (data[0] << 8)
+
+      i2c.write8(3,0)
+      time.sleep(0.0002)
+      data[2] = i2c.readU8(3)
+      time.sleep(0.0002)
+      i2c.write8(4,0)
+      time.sleep(0.0002)
+      data[3] = i2c.readU8(4)
+      msg.y_axis[0] = data[3] + (data[2] << 8)
+
+      i2c.write8(5,0)
+      time.sleep(0.0002)
+      data[4] = i2c.readU8(5)
+      time.sleep(0.0002)
+      i2c.write8(6,0)
+      time.sleep(0.0002)
+      data[5] = i2c.readU8(6)
+      msg.z_axis[0] = data[5] + (data[4] << 8)
 
       msg.id[0] = 0
-      msg.x_axis[0] = (data[0] << 8) + data[1]
-      msg.y_axis[0] = (data[2] << 8) + data[3]
-      msg.z_axis[0] = (data[4] << 8) + data[5]
       
       # publish data out
       pub.publish(msg)
@@ -55,7 +80,6 @@ class Mag_Driver():
 # Main function
 if __name__ == '__main__':
   rospy.init_node('mag3110_driver')
-  print "calling main"
   # Go to class functions that do all the heavy lifting. Do error checking.
   md = Mag_Driver()  
   try:
